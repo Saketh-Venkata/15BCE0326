87978d152a0ea6bfbe7acc47e19180d2395ffcf4
Rishavgoyal98 <33192020+Rishavgoyal98@users.noreply.github.com>
2017-11-05 00:27:42 +0530

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,1 +1,10 @@
 import java.util.ArrayList;
+class Node {
+	int name,color;		
+	ArrayList<Node> adjList;
+	
+	Node(int name) {
+		this.name = name;
+		color=-1;
+		adjList=new ArrayList<Node>();
+	}
