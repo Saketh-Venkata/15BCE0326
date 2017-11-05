71fb838e5e5267da4b344149ddc83fb01b2bd1af
mishraj343 <jayesh.mishra2015@vit.ac.in>
2017-11-05 22:47:11 +0530

    Update Vertexsetvbl.java

--- Vertexsetvbl.java
+++ Vertexsetvbl.java
@@ -1,9 +1,15 @@
 public class VertexSetVbl implements Vbl {
 	TreeMap<Integer, Node> vertexSetTemp;
+	
+  public VertexSetVbl() {
+		vertexSetTemp=new TreeMap<Integer, Node>();
+	}
   
   
-  
-  
+  public void reduce(Vbl vbl) {
+		VertexSetVbl custVbl=(VertexSetVbl) vbl
+		vertexSetTemp.put(custVbl.vertexSetTemp);
+	}
   
   
   
