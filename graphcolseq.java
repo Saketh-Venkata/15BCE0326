c071a1172a4298e2f32f9c29268acde763734ed6
Rishavgoyal98 <33192020+Rishavgoyal98@users.noreply.github.com>
2017-11-05 16:27:22 +0530

    Update graphcolseq.java

--- graphcolseq.java
+++ graphcolseq.java
@@ -3,5 +3,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.TreeMap;
-
 import edu.rit.pj2.Task;
+
+public class GraphColSeq extends Task{
+	int noOfVertices,noOfEdges;
+	TreeMap<Integer, Node> vertexSet;
