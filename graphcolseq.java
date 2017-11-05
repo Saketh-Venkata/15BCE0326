c4b15cd211ee4107e8187517f1be0bfaa4c70a22
Rishavgoyal98 <33192020+Rishavgoyal98@users.noreply.github.com>
2017-11-05 18:49:11 +0530

    Update graphcolseq.java

--- graphcolseq.java
+++ graphcolseq.java
@@ -14,5 +14,6 @@ public void main(String[] args) throws NumberFormatException, IOException {
 		}
 	GraphColSeq graphColSeq=new GraphColSeq();
 		Graph graph=graphColSeq.readInput(args[0]);
-		
+		Long start=System.currentTimeMillis();
+		graphColSeq.colorVertices(graph);
 
