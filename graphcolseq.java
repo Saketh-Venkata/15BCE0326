bd2bc9402ca67bcc0df2b58cef5175b66e9f334e
Rishavgoyal98 <33192020+Rishavgoyal98@users.noreply.github.com>
2017-11-05 16:43:03 +0530

    Update graphcolseq.java

--- graphcolseq.java
+++ graphcolseq.java
@@ -8,3 +8,7 @@ import edu.rit.pj2.Task;
 public class GraphColSeq extends Task{
 	int noOfVertices,noOfEdges;
 	TreeMap<Integer, Node> vertexSet;
+public void main(String[] args) throws NumberFormatException, IOException {
+		if (args.length != 1) {
+			usage();
+		}
